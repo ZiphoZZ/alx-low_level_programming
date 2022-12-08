ab5c29ac5cd1acc85c9945d2c64c60fd48a0df31
ZiphoZZ <nkunkuzazz@gmail.com>
2022-12-08 06:01:39 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 alwaya
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a pieca of art,\n");
+	return (0);
+}
