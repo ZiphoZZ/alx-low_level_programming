a747565f41d3245c1b57831eeba2cf2817b95ac9
ZiphoZZ <nkunkuzazz@gmail.com>
2022-12-11 02:23:41 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to base 10.
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar("%d", i);
+		i++;
+	}
+
+	putchar("\n");
+
+	return (0);
+}
