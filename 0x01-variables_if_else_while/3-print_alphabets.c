46e334491a016708a64cfd2229df1b6a38fe5862
ZiphoZZ <nkunkuzazz@gmail.com>
2022-12-11 01:19:00 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets from lowercase to uppercase
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	char low_alpha;
+	char upper_alpha;
+
+	for (low_alpha = 'a'; low_alpha <= 'z'; low_alpha++)
+	{
+		putchar(low_alpha);
+	}
+
+	for (upper_alpha = 'A'; upper_alpha <= 'Z'; upper_alpha++)
+	{
+		putchar(upper_alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
