ff013eb302752b00ca0a0031a635bc301999c4e7
ZiphoZZ <nkunkuzazz@gmail.com>
2022-12-20 17:19:45 -0800

    0.98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - int pointer that resets value to 98
+ * @n: input value
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme is for pointers, arrays and strings
