6bbf1c3a53b456587a01cb43d68c5655b3c44466
ZiphoZZ <nkunkuzazz@gmail.com>
2022-12-29 04:28:13 -0800

    1.strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @n: amount of bytes feom source
+ * @dest: destinationation
+ * @src: source
+ * Return: pointer to destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int counter = 0, counter2 = 0;
+
+	while (*(dest + counter) != '\0')
+	{
+		counter++;
+							}
+	while (counter2 < n)
+	{
+		*(dest + counter) = *(src + counter2);
+		if (*(src + counter2) == '\0')
+			break;
+		counter++;
+		counter2++;
+	}
+	return (dest);
+}
