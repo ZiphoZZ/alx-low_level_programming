0e5e0c075b8faa823c40dad68cbee5ddc8f2156c
ZiphoZZ <nkunkuzazz@gmail.com>
2023-06-08 17:52:10 -0700

    Once an idea has taken hold of the brain its alomost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /*
- * _pow_recursion - returns the value of x into power y.
+ * _pow_recursion - returns the value of "x" from the power "y"
  * @x: value of base x
  * @y: value of power y
  * Return: -1 if base case is reached.
  */
-
 
 int _pow_recursion(int x, int y)
 {
@@ -14,18 +13,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
-}
-
-int main(void)
-{
-	int x, y;
-	int power_value = _pow_recursion(x, y);
-
-	printf("Power value is %d", power_value);
-
-	return (0);
 }
